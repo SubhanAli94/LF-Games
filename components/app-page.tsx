c122ab641380fb052966a80da4f3930c7cd7099f
SubhanAli94 <subhanali.mbzuai@gmail.com>
2024-09-30 14:47:07 +0400

    Welcome is removed.

--- components/app-page.tsx
+++ components/app-page.tsx
@@ -18,7 +18,6 @@ export default async function PageComponent() {
       </header>
 
       <main className="flex-grow container mx-auto px-4 py-8">
-        <h1 className="text-4xl font-bold text-center mb-8">Welcome to LFG</h1>
         <GameShowcase />
       </main>
 
