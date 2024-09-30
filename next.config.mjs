fbf5890862bd2e8333720f2ef4972af14af0e9c0
SubhanAli94 <subhanali.mbzuai@gmail.com>
2024-09-30 11:58:05 +0400

    remove unnecassry configs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     output: 'export',
-    basePath: '/lfg',
-    assetPrefix: '/lfg',
-    trailingSlash: true,
     images: {
 
         domains: ['upload.wikimedia.org', 'img.itch.zone'],
